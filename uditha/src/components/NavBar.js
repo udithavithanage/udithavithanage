81d8dd6cdf0570e3ec4fea65bfb121bea67b6315
Uditha Vithanage <udithavithanage358@gmail.com>
2024-12-20 23:26:24 +0530

    Update NavBar.js

--- uditha/src/components/NavBar.js
+++ uditha/src/components/NavBar.js
@@ -59,7 +59,7 @@ const Navbar = () => {
           isOpen ? "translate-x-0" : "-translate-x-full"
         } transition-transform duration-300 ease-in-out md:hidden -z-40`}
       >
-        <ul className="flex flex-col items-center justify-start h-screen space-y-8 text-xl mt-20">
+        <ul className="flex flex-col items-center justify-start h-screen space-y-8 text-xl mt-30">
           {["Home", "About", "Projects", "Skills", "Contact"].map((item) => (
             <li key={item}>
               <Link
