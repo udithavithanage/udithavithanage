96b51db0db861de23032e90260087bb2fae0df3f
Uditha <udithavithanage358@gmail.com>
2025-01-11 14:16:23 +0530

    Update

--- uditha/src/App.js
+++ uditha/src/App.js
@@ -16,7 +16,31 @@ function App() {
       <Hero />
       <About />
       <Projects />
-      <AddBox />
+      <AddBox
+        logo={
+          "https://videodownloaderv2-9gvseiqgr-udithavithanages-projects.vercel.app/img/logo.png"
+        }
+        tittle={"Video Downloader"}
+        des={
+          "Video Downloader Web App A Node.js-powered video downloader for downloading videos from popular platforms like YouTube, Facebook, TikTok, and Instagram. This web app uses HBS templates, HTML, and CSS to deliver a clean, responsive interface for seamless video downloads."
+        }
+        link={
+          "https://videodownloaderv2-9gvseiqgr-udithavithanages-projects.vercel.app/"
+        }
+        color={"orange"}
+      />
+      <br />
+      <AddBox
+        logo={
+          "https://petlife-tawny.vercel.app/static/media/logo.eaec1c36dbf4107d334d.png"
+        }
+        tittle={"PetLife"}
+        des={`What if caring for your beloved pets was as effortless as the joy they bring to your life? From wagging tails to gentle purrs, our pets are family – and they deserve the very best. 🐕🐾
+That’s why we are creating PetLife, a smart Pet Healthcare Management Web App designed to make pet care seamless. With digital health records, care reminders, vet appointment scheduling, and even a community for pet lovers, we’re here to transform the way you nurture your furry friends.
+Because every bark, meow, and cuddle deserves love and care backed by innovation. 💖✨`}
+        link={"https://petlife-tawny.vercel.app/"}
+        color={"yellow"}
+      />
       <Skills />
       <Contact />
       <Footer />
--- uditha/src/components/AdBox.js
+++ uditha/src/components/AdBox.js
@@ -1,31 +1,29 @@
 import React from "react";
 
-const AdBox = () => {
+const AdBox = ({ logo, tittle, des, link, color }) => {
   return (
     <div className="px-10">
       <div className="max-w-3xl mx-auto">
         <div className="bg-white border border-gray-300 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 p-4 flex flex-col sm:flex-row items-start space-y-4 sm:space-y-0 sm:space-x-4">
           {/* Icon Section */}
-          <div className="w-16 h-16 bg-orange-100 flex items-center justify-center rounded-full mx-auto sm:mx-0">
-            <span className="text-orange-500 text-2xl font-bold">VD</span>
+          <div className="w-16 h-16 flex items-center justify-center rounded-full mx-auto sm:mx-0">
+            <span className={`text-${color}-500 text-2xl font-bold`}>
+              <img src={logo} />
+            </span>
           </div>
           {/* Content Section */}
           <div className="flex-1 text-center sm:text-left">
-            <h3 className="text-lg font-semibold text-gray-800 hover:text-orange-500 cursor-pointer transition-colors">
-              Video Downloader
-            </h3>
-            <p className="text-sm text-gray-600 mt-1">
-              Video Downloader Web App A Node.js-powered video downloader for
-              downloading videos from popular platforms like YouTube, Facebook,
-              TikTok, and Instagram. This web app uses HBS templates, HTML, and
-              CSS to deliver a clean, responsive interface for seamless video
-              downloads.
-            </p>
-            <a
-              href="https://videodownloaderv2-9gvseiqgr-udithavithanages-projects.vercel.app/"
-              className="text-orange-500 text-sm font-medium hover:underline mt-2 inline-block"
+            <h3
+              className={`text-lg font-semibold text-gray-800 hover:text-${color}-500 cursor-pointer transition-colors`}
             >
-              www.videodownloader.app
+              {tittle}
+            </h3>
+            <p className="text-sm text-gray-600 mt-1">{des}</p>
+            <a
+              href={link}
+              className={`text-gray-500 text-sm font-medium hover:underline mt-2 inline-block`}
+            >
+              {link}
             </a>
           </div>
         </div>
